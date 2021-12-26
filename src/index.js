8390098931273236cbe3521e20bffc4b3a9de9cd
Vik <dvtd2000@gmail.com>
2021-12-27 02:34:15 +0700

    Update caniuse-lite

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import store from 'app/store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import store from './app/store';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
